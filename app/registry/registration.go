112a9fedc4e9ef6be2e532be8aa0b5d3a8a78313
Art Mills <art@hamcois.com>
2023-03-12 13:57:18 -0500

    Just weekend touch point

--- app/registry/registration.go
+++ app/registry/registration.go
@@ -1,13 +1,13 @@
 package registry
 
-type Registration struct { 
-	ServiceName      ServiceName
-	ServiceURL       string
+type Registration struct {
+	ServiceName ServiceName
+	ServiceURL  string
 }
 
 type ServiceName string
 
 const (
 	LogService     = ServiceName("LogService")
+	GradingService = ServiceName("GradingService")
 )
- 
